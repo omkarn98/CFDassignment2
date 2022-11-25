acac620240186fd73f997eb9a7f080b9299d617e
IsakLundgren <isak.leopold@gmail.com>
2022-11-25 11:51:15 +0100

    bugfixing process step

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ grid_type   = 'coarse' # either 'coarse' or 'fine'
 caseID      =     3    # your case number to solve
 k           =     1   
 rho         =     1   # density
-nIterations =     1  # number of iterations
+nIterations =     2  # number of iterations
 Cp          = 500
 method = 'TDMA'
 plotVelocityVectors = False
@@ -111,10 +111,6 @@ B1 = np.zeros((nI, 1))
 B2 = np.zeros((nJ, 1))
 B3 = np.zeros((nI, 1))
 B4 = np.zeros((nJ, 1))
-x = np.zeros(nI)
-for i in range(2, nI-2):
-	x = (dxe_N[i] - dxe_N[i+1]) /(dxe_N[i+1])
-	print(x)
 velWall = 1e-4 #Tolerance for wall speed 
 for i in range(1, nI-1):
 	j = 0
@@ -187,26 +183,40 @@ for i in range(1,nI-1):
 		F[i,j,3] =  Fx_s * (rho * V[i,j]) + (1-Fx_s) * rho * V[i,j-1]  # south convective
 
 # Hybrid scheme coefficients calculations (taking into account boundary conditions)
-
-
 for i in range(1,nI-1):
 	for j in range(1,nJ-1):
 		coeffsT[i,j,0] = np.max([-F[i,j,0], D[i,j,0] - F[i,j,0]/2, 0]) #East
 		coeffsT[i,j,1] = np.max([ F[i,j,1], D[i,j,1] + F[i,j,1]/2, 0]) #West
 		coeffsT[i,j,2] = np.max([-F[i,j,2], D[i,j,2] - F[i,j,2]/2, 0]) #North
 		coeffsT[i,j,3] = np.max([ F[i,j,3], D[i,j,3] + F[i,j,3]/2, 0]) #South
-		S_p = -F[i,j,0] + F[i,j,1] - F[i,j,2] + F[i,j,3] #Correct for the hybrid scheme
-		coeffsT[i,j,4] = np.sum(coeffsT[i,j,0:3]) - S_p
 
-for j in range(1,nJ-1):
+#Boundary conditions
+
+for j in range(1, nJ-1):
 	i = 1
 	if(B4[j] == 1):
 		T[i,j] = 293
-	i = nI-2
-	if(B2[j]==2):
-		coeffsT[i,j,0] = 0
-	
+	else:
+		coeffsT[i,j,1] = 0
+		F[i,j,1] = 0
 
+	i = nI-2
+	coeffsT[i,j,0] = 0
+	F[i,j,0] = 0
+
+for i in range(1,nI-1):
+	j = 1
+	coeffsT[i,j,3] = 0
+	F[i,j,3] = 0
+
+	j = nJ-2
+	coeffsT[i,j,2] = 0
+	F[i,j,2] = 0
+	
+for i in range(1, nI-1):
+	for j in range(1, nJ-1):
+		S_p = -F[i,j,0] + F[i,j,1] - F[i,j,2] + F[i,j,3] #Correct for the hybrid scheme
+		coeffsT[i,j,4] = np.sum(coeffsT[i,j,0:3]) - S_p
 
 for iter in range(nIterations): 
     # Impose boundary conditions
@@ -248,32 +258,44 @@ for iter in range(nIterations):
 				P[i,j] = 0
 				Q[i,j] = (d[i,j] + c[i,j] * Q[i-1,j] + b[i,j] * T[i+1,j]) / (a[i,j] - c[i,j] * P[i-1,j])
 
-				for i in range(1, nI-1):
-					T[nI - i - 1,j] = P[nI - i - 1,j] * T[nI - i,j] + Q[nI - i - 1,j]
+				for i in reversed(range(1, nI-1)):
+					T[i,j] = P[i,j] * T[i+1,j] + Q[i,j]
 		else:
+			tfrlhsdajf=1
 			#Solve horizontally
-			for i in range(1,nI-1):
-				for j in range(1,nJ-1):
-					a[i,j] = coeffsT[i,j,4] #a_p
-					b[i,j] = coeffsT[i,j,2] #a_n
-					c[i,j] = coeffsT[i,j,3] #a_s
-					d[i,j] = coeffsT[i,j,0] * T[i+1,j] + coeffsT[i,j,1] * T[i-1,j] 
-				#Construct P and Q terms
-				j = 1
-				P[i,j] = b[i,j] / a[i,j]
-				Q[i,j] = (d[i,j] + c[i,j] * T[i,j-1])/a[i,j] 
-				for i in range(2,nI-2):
-					P[i,j] = b[i,j] / (a[i,j] - c[i,j] * P[i,j-1])
-					Q[i,j] = (d[i,j] + c[i,j] * Q[i,j-1]) / (d[i,j] - c[i,j] * Q[i,j-1])
-				i=nI-2
-				P[i,j] = 0
-				Q[i,j] = (d[i,j] + c[i,j] * Q[i,j-1] + b[i,j] * T[i,j+1]) / (a[i,j] - c[i,j] * P[i,j-1])
-
-				for j in range(1, nJ-1):
-					T[i,nJ - j - 1] = P[i,j] * T[i,nJ - j] + Q[i,j]
+			#for i in range(1,nI-1):
+			#	for j in range(1,nJ-1):
+			#		a[i,j] = coeffsT[i,j,4] #a_p
+			#		b[i,j] = coeffsT[i,j,2] #a_n
+			#		c[i,j] = coeffsT[i,j,3] #a_s
+			#		d[i,j] = coeffsT[i,j,0] * T[i+1,j] + coeffsT[i,j,1] * T[i-1,j] 
+			#	#Construct P and Q terms
+			#	j = 1
+			#	P[i,j] = b[i,j] / a[i,j]
+			#	Q[i,j] = (d[i,j] + c[i,j] * T[i,j-1])/a[i,j] 
+			#	for i in range(2,nI-2):
+			#		P[i,j] = b[i,j] / (a[i,j] - c[i,j] * P[i,j-1])
+			#		Q[i,j] = (d[i,j] + c[i,j] * Q[i,j-1]) / (d[i,j] - c[i,j] * Q[i,j-1])
+			#	i=nI-2
+			#	P[i,j] = 0
+			#	Q[i,j] = (d[i,j] + c[i,j] * Q[i,j-1] + b[i,j] * T[i,j+1]) / (a[i,j] - c[i,j] * P[i,j-1])
+#
+			#	for j in range(1, nJ-1):
+			#		T[i,nJ - j - 1] = P[i,nJ - j - 1] * T[i,nJ - j] + Q[i,nJ - j - 1]
 
     # Copy temperatures to boundaries
-    
+	for i in range(1, nI-1):
+		j = 1
+		T[i,j-1] = T[i,j]
+
+		j = nJ-1
+		T[i,j] = T[i,j-1]
+
+	for j in range(1, nJ-1):
+		i = 1
+		if(B4[j] != 1):
+			T[i-1,j] = T[i,j]
+
     # Compute residuals (taking into account normalization)
 	#residuals.append() # fill with your residual value for the 
                        # current iteration
@@ -297,6 +319,7 @@ plt.quiver(xv, yv, U.T, V.T)
 plt.title('Velocity vectors')
 plt.xlabel('x [m]')
 plt.ylabel('y [m]')
+
 
 plt.figure()
 plt.contourf(xv, yv, T.T)
